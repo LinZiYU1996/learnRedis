528fa797bfb4ea62759609fa37603ecb8ffa8ac1
LinZiYu1996 <2669093302@qq.com>
2020-07-31 09:02:36 +0800

    7-31

--- connect_demo/src/main/java/com/lin/connect_demo/config/RedisConfig.java
+++ connect_demo/src/main/java/com/lin/connect_demo/config/RedisConfig.java
@@ -2,7 +2,14 @@ package com.lin.connect_demo.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
 
 /**
  * \* Created with IntelliJ IDEA.
@@ -14,14 +21,20 @@ import org.springframework.context.annotation.Configuration;
  */
 
 @Configuration
-@EnableCaching
+//@EnableCaching
 //@RefreshScope
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
-    private String host;
-    @Value("${spring.redis.port}")
-    private int port;
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
+
 
 
 }
--- connect_demo/src/test/java/com/lin/connect_demo/ConnectDemoApplicationTests.java
+++ connect_demo/src/test/java/com/lin/connect_demo/ConnectDemoApplicationTests.java
@@ -1,13 +1,69 @@
 package com.lin.connect_demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
+@Slf4j
 class ConnectDemoApplicationTests {
 
     @Test
     void contextLoads() {
     }
+    @Autowired
+    private RedisTemplate<String, String> strRedisTemplate;
+
+    @Test
+    public void testString() {
+        strRedisTemplate.opsForValue().set("strKey", "zwqh");
+        System.out.println(strRedisTemplate.opsForValue().get("strKey"));
+    }
+
+
+    @Test
+    public void t1() {
+        strRedisTemplate.opsForValue().set("a", "1");
+        strRedisTemplate.opsForValue().increment("a", 100L);
+        log.info(strRedisTemplate.opsForValue().get("a"));
+
+    }
+
+
+    @Test
+    public void t2() {
+        strRedisTemplate.opsForValue().append("b", "123");
+        log.info(strRedisTemplate.opsForValue().get("b"));
+        strRedisTemplate.opsForValue().getBit("c",12);
+        log.info(strRedisTemplate.opsForValue().get("c"));
+    }
+
+    @Test
+    public void t3() {
+
+        strRedisTemplate.opsForValue().set("a1","abc",20);
+        log.info(strRedisTemplate.opsForValue().get("a1"));
+
+
+    }
+
+    @Test
+    public void t4() {
+
+        strRedisTemplate.opsForValue().set("a2", "456");
+        Long s = strRedisTemplate.opsForValue().size("a2");
+//        log.info(s);
+
+        System.out.println(s);
+        // 3
+        
+
+
+    }
+
+
+
 
 }
