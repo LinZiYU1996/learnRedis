46752f6bb2cb3755c3724c899c04f9fd083da504
LinZiYu1996 <2669093302@qq.com>
2020-08-18 21:08:40 +0800

    8-18

---
+++ learn-aop-blog-demo/src/main/java/com/lin/learnaopblogdemo/LearnAopBlogDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lin.learnaopblogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAopBlogDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnAopBlogDemoApplication.class, args);
+    }
+
+}
---
+++ learn-aop-blog-demo/src/test/java/com/lin/learnaopblogdemo/LearnAopBlogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.learnaopblogdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAopBlogDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
